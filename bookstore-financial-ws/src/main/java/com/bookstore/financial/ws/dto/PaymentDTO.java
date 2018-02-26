c3ca39c790f39664b229a22d747ea72f842fafac
Lau <elauso@gmail.com>
2018-02-26 02:14:08 -0300

    Adjusting validation contract

--- bookstore-financial-ws/src/main/java/com/bookstore/financial/ws/dto/PaymentDTO.java
+++ bookstore-financial-ws/src/main/java/com/bookstore/financial/ws/dto/PaymentDTO.java
@@ -5,7 +5,6 @@ import java.util.Calendar;
 import java.util.List;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
@@ -15,8 +14,6 @@ import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import com.bookstore.financial.core.enumeration.PaymentType;
 import com.bookstore.libraries.adapter.DateTimeAdapter;
-import com.bookstore.libraries.validation.annotation.CollectionValid;
-import com.bookstore.libraries.validation.annotation.ProductCodeValid;
 import com.bookstore.libraries.validation.annotation.UnitCodeValid;
 import com.bookstore.libraries.ws.AbstractDTO;
 
@@ -32,12 +29,10 @@ public class PaymentDTO extends AbstractDTO {
 	@XmlElement(required=true)
 	private BigDecimal value;
 	
-	@NotNull
 	@XmlElement(required=false)
 	@XmlJavaTypeAdapter(DateTimeAdapter.class)
 	private Calendar date;
 	
-	@CollectionValid(elementType=String.class, constraints={ProductCodeValid.class})
 	@XmlElementWrapper(name = "products", required=true)
 	@XmlElement(name="code", required=true)
 	private List<String> productCodes;
